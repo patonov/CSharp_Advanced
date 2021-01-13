b32b83f144c7f7dce6a3e12dc7fe9974ffc5d269
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-13 22:25:36 +0200

    Add files via upload

---
+++ Reverse Strings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var charStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var temp = input[i];
+                charStack.Push(temp);
+            }
+            while (charStack.Count > 0)
+            {
+                Console.Write(charStack.Pop());
+            }
+        }
+    }
+}

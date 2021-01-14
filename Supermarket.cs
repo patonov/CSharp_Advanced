616e0965daf90e952150c45fa8f6bd0dcdae5192
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-14 18:44:59 +0200

    Add files via upload

---
+++ Supermarket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var queueOfNums = new Queue<string>();
+
+            while (name != "End")
+            {
+
+                if (name != "Paid")
+                {
+                    queueOfNums.Enqueue(name);
+                }
+                else
+                {
+                    while (queueOfNums.Any())
+                    {
+                        Console.WriteLine(queueOfNums.Dequeue());
+                    }
+                }
+
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("{0} people remaining.", queueOfNums.Count);
+
+        }
+    }
+}

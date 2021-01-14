afb0b3eff3cd81ba0cdc5e5e3d88371e23fffef5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-14 18:47:27 +0200

    Add files via upload

---
+++ Hot Potato.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            var queueOfNums = new Queue<string>();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                queueOfNums.Enqueue(name[i]);
+            }
+
+            while (queueOfNums.Count != 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    queueOfNums.Enqueue(queueOfNums.Dequeue());
+                }
+                Console.WriteLine("Removed {0}", queueOfNums.Dequeue());
+            }
+
+            Console.WriteLine("Last is {0}", queueOfNums.Dequeue());
+
+        }
+    }
+}

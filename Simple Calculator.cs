78a9b6383be466eee7f0890c89c0247c2f2e300d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-14 18:42:46 +0200

    Add files via upload

---
+++ Simple Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().ToArray();
+            var numStack = new Stack<string>(numbers.Reverse());
+
+            while (numStack.Count > 1)
+            {
+                int firstNum = int.Parse(numStack.Pop());
+                string sign = numStack.Pop().ToString();
+                int secondNum = int.Parse(numStack.Pop());
+
+                switch (sign)
+                {
+                    case "+":
+                        numStack.Push((firstNum + secondNum).ToString());
+                        break;
+                    case "-":
+                        numStack.Push((firstNum - secondNum).ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(numStack.Pop());
+
+        }
+    }
+}

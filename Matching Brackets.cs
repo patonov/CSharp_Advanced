6e15d6484fafe86f479ed5426aac7bf742f97c0e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-14 18:43:12 +0200

    Add files via upload

---
+++ Matching Brackets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine();
+            var stackOfIndexes = new Stack<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == '(')
+                {
+                    stackOfIndexes.Push(i);
+                }
+                else if (numbers[i] == ')')
+                {
+                    int startInd = stackOfIndexes.Pop();
+                    Console.WriteLine(numbers.Substring(startInd, i - startInd + 1));
+                }
+            }
+
+        }
+    }
+}

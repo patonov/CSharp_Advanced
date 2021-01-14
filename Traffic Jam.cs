2868a181c813a6bee6b660a2c62b44c5ae0d2b9c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-14 18:45:23 +0200

    Add files via upload

---
+++ Traffic Jam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string car = Console.ReadLine();
+
+            int count = 0;
+            var queueOfCars = new Queue<string>();
+
+            while (car != "end")
+            {
+                if (car == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queueOfCars.Count > 0)
+                        {
+                            Console.WriteLine("{0} passed!", queueOfCars.Dequeue());
+                            count++;
+                        }
+                    }
+
+                }
+                else
+                {
+                    queueOfCars.Enqueue(car);
+                }
+
+                car = Console.ReadLine();
+            }
+            Console.WriteLine("{0} cars passed the crossroads.", count);
+        }
+    }
+}

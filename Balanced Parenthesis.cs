8ef633c0040da03bf9ad90c20a046bfc367abae7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-16 21:57:59 +0200

    Add files via upload

---
+++ Balanced Parenthesis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parentheses = Console.ReadLine();
+            var firstPart = parentheses.Substring(0, parentheses.Length / 2);
+            var secondPart = parentheses.Substring(parentheses.Length / 2, parentheses.Length / 2);
+            var firstPartStack = new Stack<char>(firstPart);
+            var secondPartQueue = new Queue<char>(secondPart);
+            bool isNice = true;
+
+            while (firstPartStack.Any())
+            {
+                char currentFirstPartChar = firstPartStack.Pop();
+                char currentSecondPartChar = secondPartQueue.Dequeue();
+                if (currentFirstPartChar == '(' && currentSecondPartChar == ')')
+                {
+                    continue;
+                }
+                else if (currentFirstPartChar == '[' && currentSecondPartChar == ']')
+                {
+                    continue;
+                }
+                else if (currentFirstPartChar == '{' && currentSecondPartChar == '}')
+                {
+                    continue;
+                }
+                else
+                {
+                    isNice = false;
+                    break;
+                }
+
+            }
+            if (isNice)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}

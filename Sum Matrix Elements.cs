2fd8d2782a18f3375fb0e132e44885146ec7f05d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-18 19:08:27 +0200

    Add files via upload

---
+++ Sum Matrix Elements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixParameters = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int rows = matrixParameters[0];
+            int cols = matrixParameters[1];
+            int matrixSum = 0;
+
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            foreach (var element in matrix)
+            {
+                matrixSum += element;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(matrixSum);
+        }
+    }
+}

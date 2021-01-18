b76f546d553fb223070ec98b6ad91dbf373c90d1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-18 19:09:38 +0200

    Add files via upload

---
+++ Symbol in Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] chars = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = chars[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine("({0}, {1})", row, col);
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("{0} does not occur in the matrix", symbol);
+
+
+        }
+    }
+}

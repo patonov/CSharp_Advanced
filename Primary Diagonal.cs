a531a22301bbd40c0b4305c3501d9eeee315c2d0
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-18 19:09:15 +0200

    Add files via upload

---
+++ Primary Diagonal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+            int diagonalSum = 0;
+
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                diagonalSum += matrix[row, row];
+            }
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}

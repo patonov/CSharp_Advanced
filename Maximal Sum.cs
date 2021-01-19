46a24a7c0a9cc780f68d86fdb67732db6ae21746
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-19 18:12:52 +0200

    Add files via upload

---
+++ Maximal Sum.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = n[0];
+            int cols = n[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int rowIndex = -1;
+            int colIndex = -1;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int sum = matrix[row, col];
+                    sum += matrix[row, col + 1];
+                    sum += matrix[row, col + 2];
+
+                    sum += matrix[row + 1, col];
+                    sum += matrix[row + 1, col + 1];
+                    sum += matrix[row + 1, col + 2];
+
+                    sum += matrix[row + 2, col];
+                    sum += matrix[row + 2, col + 1];
+                    sum += matrix[row + 2, col + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        rowIndex = row;
+                        colIndex = col;
+                    }
+                }
+            }
+            Console.WriteLine("Sum = {0}", maxSum);
+            for (int row = rowIndex; row < rowIndex + 3; row++)
+            {
+                for (int col = colIndex; col < colIndex + 3; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}

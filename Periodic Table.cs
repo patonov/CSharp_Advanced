f5961be1f019c60fe01a1c6284d0d9a5eaa47fee
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-23 20:40:54 +0200

    Add files via upload

---
+++ Periodic Table.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            HashSet<string> chemicalSet = new HashSet<string>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    chemicalSet.Add(input[j]);
+                }
+
+            }
+
+            foreach (var item in chemicalSet.OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+
+
+        }
+    }
+}

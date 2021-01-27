36b3fa2015c03020016c02f5917809b0e8b6dc66
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-27 21:55:14 +0200

    Add files via upload

---
+++ Sum Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numArr = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            Console.WriteLine(numArr.Length);
+            Console.WriteLine(numArr.Sum());
+
+        }
+    }
+}

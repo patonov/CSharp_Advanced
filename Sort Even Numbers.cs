5bd5e2855d15ef5c9fb3eb8b50aa3f3b5c5464c6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-27 21:44:22 +0200

    Add files via upload

---
+++ Sort Even Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}

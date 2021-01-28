a134da3e7a00c849d8a7302c64a4618a05ba7419
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-28 18:34:20 +0200

    Add files via upload

---
+++ Add VAT Functional.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_VAT_Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var priceArr = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).Select(n => n * 1.20).ToArray();
+
+            foreach (var price in priceArr)
+            {
+                Console.WriteLine("{0:f2}", price);
+            }
+
+        }
+    }
+}

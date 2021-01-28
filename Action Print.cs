bbc2a42ffe05c80f87689f77e4b4d7d6ac07f2dd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-28 19:50:25 +0200

    Add files via upload

---
+++ Action Print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            var nameArr = Console.ReadLine().Split().ToArray();
+
+            printNames(nameArr);
+
+        }
+    }
+}

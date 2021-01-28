0f94fdd251bd7266e3eb2a1723c5c09bd73b106a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-28 18:33:23 +0200

    Add files via upload

---
+++ Sum Numbers Functional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Numbers_Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = n => int.Parse(n);
+            var numArr = Console.ReadLine().Split(',').Select(parser).ToArray();
+            Console.WriteLine(numArr.Length);
+            Console.WriteLine(numArr.Sum());
+        }
+    }
+}

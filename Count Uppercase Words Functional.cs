3be6aafd9a1120643433f9c8df03c7a2c876dfd4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-28 18:33:49 +0200

    Add files via upload

---
+++ Count Uppercase Words Functional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Uppercase_Words_Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> upperCaseChecker = n => n[0] == n.ToUpper()[0];
+
+            var wordArr = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Where(upperCaseChecker).ToArray();
+
+            foreach (var word in wordArr)
+            {
+                Console.WriteLine(word);
+            }
+
+
+        }
+    }
+}

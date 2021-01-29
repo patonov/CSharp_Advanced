0a441687234fb7d57367b517b890311599bd3996
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-29 17:12:16 +0200

    Add files via upload

---
+++ Custom Min Function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> minValuePrint = nums =>
+            {
+                int minN = int.MaxValue;
+
+                foreach (int n in nums)
+                {
+                    if (n < minN)
+                    {
+                        minN = n;
+                    }
+                }
+
+                return minN;
+            };
+
+            int result = minValuePrint(numArr);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}

713eeeaa83f360f1d748d5c5117419a0a636252c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-29 17:12:49 +0200

    Add files via upload

---
+++ Knights of Honor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            var nameArr = Console.ReadLine().Split().Select(name => $"Sir {name}").ToArray();
+
+            printNames(nameArr);
+
+        }
+    }
+}

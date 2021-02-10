30a316ef4ea78408511041073254e8bd55f04d26
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-10 20:55:51 +0200

    Add files via upload

---
+++ Reverse and Exclude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int denominator = int.Parse(Console.ReadLine());
+
+            nums.Reverse();
+
+            nums = nums.Where(x => x % denominator != 0).ToList();
+
+            Action<List<int>> print = numbers => Console.WriteLine(string.Join(" ", numbers));
+            print(nums);
+
+        }
+    }
+}

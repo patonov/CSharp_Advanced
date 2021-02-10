9c38b6c9a3c76aea4691c78e41e3ed1f3a6bc4c0
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-10 20:56:13 +0200

    Add files via upload

---
+++ Predicate for Names.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            names = names.Where(name => name.Length <= length).ToList();
+
+            Action<List<string>> print = ns => Console.WriteLine(string.Join(Environment.NewLine, ns));
+
+            print(names);
+                
+
+        }
+    }
+}

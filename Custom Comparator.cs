a4c753140feb7761e9f0accc6d064a87c386abbb
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-10 20:56:38 +0200

    Add files via upload

---
+++ Custom Comparator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(nums, (a, b) =>
+            {
+                if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+                else if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+                int temp = a.CompareTo(b);
+                return temp;
+                                               
+            });
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}

106148555e009268fee70b3ec62c520cc5c45f14
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-05 17:33:59 +0200

    Add files via upload

---
+++ TriFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charSumTarget = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValid = (name, number) => name.ToCharArray()
+                .Select(ch => (int)ch).Sum() >= number;
+
+            Func<string[], int, Func<string, int, bool>, string> validName = (array, number, func) => array
+                .FirstOrDefault(name => func(name, number));
+
+            string output = validName(names, charSumTarget, isValid);
+            Console.WriteLine(output);
+        }
+    }
+}

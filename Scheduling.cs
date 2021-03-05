5089304e975393cc3c0dd1222c0c2066cab3ed8e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-05 17:24:42 +0200

    Add files via upload

---
+++ Scheduling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tasks = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int[] threads = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int targetTask = int.Parse(Console.ReadLine());
+
+            Stack<int> taskStack = new Stack<int>(tasks);
+            Queue<int> threadQueue = new Queue<int>(threads);
+
+            while (true)
+            {
+
+                int currentTask = taskStack.Peek();
+                int currentThread = threadQueue.Peek();
+                if (currentTask == targetTask)
+                {
+                    Console.WriteLine("Thread with value {0} killed task {1}", currentThread, targetTask);
+                    break;
+                }
+
+                if (currentThread >= currentTask)
+                {
+                    taskStack.Pop();
+                    threadQueue.Dequeue();
+                }
+                else
+                {
+                    threadQueue.Dequeue();
+                }
+            }
+
+            Console.Write(string.Join(" ", threadQueue));
+            Console.WriteLine();
+
+        }
+    }
+}

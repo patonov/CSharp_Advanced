eeea58c2bf0e67f210acb98fcab80db1ecb19e0b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-31 22:46:53 +0300

    Add files via upload

---
+++ Birthday_Celebration_ExamProblem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Celebration_ExamProblem
+{
+    class Birthday_Celebration_ExamProblem
+    {
+        static void Main(string[] args)
+        {
+            List<int> guestsEatTooMuch = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> platesForGuests = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Stack<int> plateStack = new Stack<int>(platesForGuests);
+            Queue<int> guestQueue = new Queue<int>(guestsEatTooMuch);
+            int waste = 0;
+
+            while (true)
+            {
+                var currentPlate = plateStack.Pop();
+
+                if (currentPlate < guestQueue.Peek())
+                {
+                    var currentGuest = guestQueue.Dequeue();
+                    currentGuest -= currentPlate;
+
+                    while (true)
+                    {
+                        var lastPlate = plateStack.Pop();
+                        if (lastPlate < currentGuest)
+                        {
+                            currentGuest -= lastPlate;
+                        }
+                        else
+                        {
+                            waste += lastPlate - currentGuest;
+                            break;
+                        }
+
+                        if (plateStack.Count == 0)
+                        {
+                            Console.WriteLine("Guests: {0}", String.Join(" ", guestQueue));
+                            Console.WriteLine("Wasted grams of food: {0}", waste);
+                        }
+                    }
+                }
+
+                else if (currentPlate >= guestQueue.Peek())
+                {
+                    waste += currentPlate - guestQueue.Peek();
+                    guestQueue.Dequeue();
+                }
+
+                if (guestQueue.Count == 0)
+                {
+                    Console.WriteLine("Plates: {0}", String.Join(" ", plateStack));
+                    Console.WriteLine("Wasted grams of food: {0}", waste);
+                    return;
+                }
+                else if (plateStack.Count == 0)
+                {
+                    Console.WriteLine("Guests: {0}", String.Join(" ", guestQueue));
+                    Console.WriteLine("Wasted grams of food: {0}", waste);
+                    return;
+                }
+
+            }
+        }
+    }
+}

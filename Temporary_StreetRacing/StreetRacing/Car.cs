d7dedbcf71fb12fdf08c1e11c8b6c2482e82c491
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-03 23:21:08 +0300

    Add files via upload

---
+++ Temporary_StreetRacing/StreetRacing/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private string licensePlate;
+        private int horsePower;
+        private double weight;
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public override ToString()
+        {
+            return $"Make: {this.Make}\r\n" +
+                    $"Model: {this.Model}\r\n" +
+                    $"License Plate: {this.LicensePlate}\r\n" +
+                    $"Horse Power: {this.HorsePower}\r\n" +
+                    $"Weight: {this.Weight}\r\n";
+        }
+
+
+    }
+}
---
+++ Temporary_StreetRacing/StreetRacing/Race.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        private Dictionary<string, Car> Participants;
+        private string name;
+        private string type;
+        private int laps;
+        private int capacity;
+        private int maxHorsePower;
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.Participants = new Dictionary<string, Car>();
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+
+        public int Count => this.Participants.Count;
+
+        public static void Add(Car car)
+        {
+            if (!this.Participants.ContainsKey(car.LicensePlate) &&
+                this.Count < this.Capacity &&
+                car.HorsePower < this.MaxHorsePower)
+            {
+                this.Participants.Add(car.LicensePlate, car);
+            }
+        }
+
+        public static bool Remove(string licensePlate)
+        {
+            if (this.Participants.ContainsKey(licensePlate))
+            {
+                Car car = car.LicensePlate.Where(c => c.LicensePlate == licensePlate);
+                this.Participants.Remove(licensePlate, car);
+                return true;
+            }
+            return false;
+        }
+    }
+}

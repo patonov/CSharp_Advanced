586ab2052c6a49a2866781b3f949cf451eef6361
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-08-24 19:42:41 +0300

    Add files via upload

--- DelegatesDemo/Program.cs
+++ DelegatesDemo/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace DelegatesDemo
+﻿using System.Linq;
+using System.Collections;
+
+namespace DelegatesDemo
 {
     internal class Program
     {
@@ -18,13 +21,29 @@
             func = (x, y) => x * y;
             //Console.WriteLine(func(x, y));
 
-            Action<string> evenNumsPrinter = s => Console.WriteLine(s);
+            //Action<string> evenNumsPrinter = s => Console.WriteLine(s);
 
-            evenNumsPrinter(string.Join(" ", Console.ReadLine()!.Split().Select(n => int.Parse(n))
-                .Where(n => n % 2 == 0)
-                .OrderBy(n => n)
-                .ToArray()
-                ));
+            //evenNumsPrinter(string.Join(" ", Console.ReadLine()!.Split().Select(n => int.Parse(n))
+            //  .Where(n => n % 2 == 0)
+            //.OrderBy(n => n)
+            //.ToArray()
+            //));
+
+            Predicate<int> isNegative = x => x < 0;
+            //Console.WriteLine(isNegative(int.Parse(Console.ReadLine()!)));
+
+           // Console.WriteLine(string.Join(" ", Console.ReadLine()!.Split()
+             //   .Select(n => int.Parse(n))
+               // .Where(x => isNegative(x))
+               // .ToArray()));
+
+            Predicate<string> hasCapitalizedFirstLetter = w => w[0].ToString() == w[0].ToString().ToUpper();
+
+            Predicate<string> hasCapitalizedLastLetter = w => char.IsUpper(w[w.Length - 1]);
+
+            Console.WriteLine(hasCapitalizedFirstLetter(Console.ReadLine()!));
+            Console.WriteLine(hasCapitalizedLastLetter(Console.ReadLine()!));
+
 
 
         }
